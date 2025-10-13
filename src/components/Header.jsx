cd47d5359db674494081db7ad4575d9668687bdb
Tanmoy Mozumder <tanmoymozumder0@gmail.com>
2025-10-13 21:24:45 +0600

    First Phase Only completed Hero Section and navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Hero from './Hero'
+
+const Header = () => {
+    return (
+        <>
+            <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden' id='Header' style={{ backgroundImage: "url('/public/header_img.png')" }}>
+                <Navbar />
+                <Hero
+                    heroText='Explore homes that fit your dreams'
+                    buttonText1='Projects'
+                    buttonText2='Contact Us'
+                />
+            </div>
+        </>
+    )
+}
+
+export default Header
---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Hero = ({ heroText, buttonText1, buttonText2 }) => {
+    return (
+        <>
+            <div className='container mx-auto py-4 px-6 md:px-20 lg:px-32 text-center  text-white'>
+                <h2 className='text-5xl sm:text-6xl md:text[82px] inline-block max-w-3xl font-semibold pt-20'>
+                    {heroText}
+                </h2>
+
+                <div className='space-x-6 mt-16'>
+                    <a href="#Projects" className='border border-white px-8 py-3 rounded'>{buttonText1}</a>
+                    <a href="#Contact" className='bg-blue-500 px-8 py-3 rounded'>{buttonText2}</a>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default Hero
